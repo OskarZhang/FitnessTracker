ef472cd5f8c8ddff70c7f726bd463f46ed5420b1
Oskar Zhang <3388349+OskarZhang@users.noreply.github.com>
2025-10-18 00:07:44 -0700

    modal polish

--- FitnessTracker/Views/AddWorkout/AddWorkoutView.swift
+++ FitnessTracker/Views/AddWorkout/AddWorkoutView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct AddWorkoutView: View {
     @StateObject var viewModel: AddWorkoutViewModel
     @Binding var isPresented: Bool
+    @State private var sheetHeight: CGFloat = .zero
 
     init(isPresented: Binding<Bool>) {
         self._isPresented = isPresented
@@ -20,21 +21,26 @@ struct AddWorkoutView: View {
             VStack(alignment: .leading) {
                 Text("First time? ")
                     .multilineTextAlignment(.leading)
-                    .font(.largeTitle)
+                    .font(.title)
+                    .fontWeight(.medium)
+                    .padding(.top)
                 Text("GTFG AI can recommend you a full set based on your biometrics.")
                     .multilineTextAlignment(.leading)
                     .font(.subheadline)
-                Spacer()
+                    .fixedSize(horizontal: false, vertical: true)
+                
+                Spacer(minLength: 16)
                 Button {
                     viewModel.showNewExerciseOnboarding = false
                     viewModel.generateWeightAndSetSuggestion()
                 } label: {
                     Text("Generate full set")
+                        .fontWeight(.semibold)
                         .frame(maxWidth: .infinity)
                         .frame(height: 30)
                 }
                 .tint(Color.bratGreen)
-                .buttonStyle(.borderedProminent)
+                .buttonStyle(.glassProminent)
                 
                 Button() {
                     viewModel.showNewExerciseOnboarding = false
@@ -42,13 +48,30 @@ struct AddWorkoutView: View {
                     Text("Nah! Let me log my own")
                         .frame(maxWidth: .infinity)
                         .frame(height: 30)
+                        .fontWeight(.semibold)
                         .foregroundStyle(Color.bratGreen)
                 }
-                .buttonStyle(.bordered)
+                .buttonStyle(.glass)
             }
             .padding()
-            .presentationDetents([.height(240)])
+            .overlay {
+                GeometryReader { geometry in
+                    Color.clear.preference(key: InnerHeightPreferenceKey.self, value: geometry.size.height)
+                }
+            }
+            .onPreferenceChange(InnerHeightPreferenceKey.self) { newHeight in
+                sheetHeight = newHeight
+            }
+            .presentationDetents([.height(sheetHeight)])
+
         })
         .accentColor(.bratGreen)
+    }
+}
+
+struct InnerHeightPreferenceKey: PreferenceKey {
+    static let defaultValue: CGFloat = .zero
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
     }
 }
